ff8bd96247c495b534b1b4676adac6fc492d4591
Sarumathin1311 <59573803+Sarumathin1311@users.noreply.github.com>
2020-08-26 19:57:25 +0530

    Add files via upload

---
+++ Registrationservice/src/main/java/com/registration/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.registration.constants;
+
+class Constants {
+	
+	public static final String AddAccountUrl="http://BankAccountservice/addAccount";
+	public static final String AddMutualFundUrl="";
+	public static final String GetAccountUrl="";
+	public static final String GetMutualFundUrl="";
+	public static final String GetInvestmentDetailsUrl="";
+	
+
+}
